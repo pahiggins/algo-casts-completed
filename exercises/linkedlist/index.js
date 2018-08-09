2997df627640cfaff457e770e554a52c3ed8cd47
Peter A. Higgins <pahiggins@cenabo.com>
2018-08-09 14:04:21 +0100

    linkedlist solution part 1

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,84 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    size() {
+        let counter = 0;
+        let node = this.head;
+
+        while (node) {
+            counter++;
+            node = node.next;
+        }
+
+        return counter;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) {
+            return;
+        } else {
+            this.head = this.head.next;
+        }
+    }
+
+    removeLast() {
+        if (this.size() === 0) {
+            return;
+        }
+
+        if (this.size() === 1) {
+            return this.head = null;
+        }
+
+        let previous = this.head;
+        let node = this.head.next;
+
+        while (node) {
+            if (!node.next) {
+                previous.next = null;
+            }
+            previous = previous.next;
+            node = node.next;
+        }
+    }
+}
 
 module.exports = { Node, LinkedList };
