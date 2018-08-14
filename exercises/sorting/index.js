648c4539f2c143acda939f3c016882ec160dd288
Peter A. Higgins <pahiggins@cenabo.com>
2018-08-14 12:10:45 +0100

    sorting solution part 1

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,17 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr.length - i - 1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                const smaller = arr[j + 1];
+                const larger = arr[j];
+                arr[j] = smaller;
+                arr[j + 1] = larger;
+            }
+        }
+    }
+    return arr;
 }
 
 function selectionSort(arr) {
