2d1bad84b4dfe951448cb2bf2e57c4506c3a7fc0
Peter A. Higgins <pahiggins@cenabo.com>
2018-08-14 13:56:13 +0100

    sorting solution part 3

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -37,11 +37,37 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
 
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center);
+
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+    let results = [];
 
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+
+    if (left.length) {
+        results = [...results, ...left];
+    }
+
+    if (right.length) {
+        results = [...results, ...right];
+    }
+
+    return results;
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
